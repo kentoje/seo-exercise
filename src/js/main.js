5836ba3a9b2893d5b7ae19664e18cdb567e59359
kentoje <k.monthubert@gmail.com>
2019-12-18 23:37:53 +0100

    hide header on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-console.log('Hello')
+const header = document.querySelector('.header')
+let newValue = 0
+
+const detectScrollDirection = target => {
+  let oldValue = 0
+
+  document.addEventListener('scroll', _ => {
+    newValue = window.pageYOffset;
+
+    (oldValue - newValue) < 0
+      ? target.classList.add('hidden')
+      : target.classList.remove('hidden');
+
+    oldValue = newValue;
+  })
+}
+
+detectScrollDirection(header)
